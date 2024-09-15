f79891866698be0079f06d8d7f93d34bb582cc97
Candela Salvatto <cande_salvatto12@hotmail.com>
2024-09-14 21:06:29 -0300

    quitando list style

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -28,14 +28,14 @@ export const Products = () => {
     infinite: true,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToScroll: 0,
     initialSlide: 0,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 4,
-          slidesToScroll: 0,
+          slidesToShow: 3,
+          slidesToScroll: 1,
         }
       },
       {
